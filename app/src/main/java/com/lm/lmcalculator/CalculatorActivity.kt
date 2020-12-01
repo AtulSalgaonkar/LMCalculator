909a13cc7f1e9e3d80e82a68661169792d4a1756
Legend-Mortal <atul.salgaonkar14@gmail.com>
2020-12-02 00:28:35 +0530

    full calculator feature commit

--- app/src/main/java/com/lm/lmcalculator/CalculatorActivity.kt
+++ app/src/main/java/com/lm/lmcalculator/CalculatorActivity.kt
@@ -1,160 +1,285 @@
 package com.lm.lmcalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import androidx.databinding.DataBindingUtil
+import android.text.TextUtils
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProviders
-import com.lm.lmcalculator.databinding.ActivityCalculatorBinding
+import kotlinx.android.synthetic.main.activity_calculator.*
 
 class CalculatorActivity : AppCompatActivity() {
 
-    var mCalculatorBinding: ActivityCalculatorBinding? = null
-    var calculateTv: TextView? = null
-    var resultTv: TextView? = null
-    var oneTv: TextView? = null
-    var twoTv: TextView? = null
-    var threeTv: TextView? = null
-    var fourTv: TextView? = null
-    var fiveTv: TextView? = null
-    var sixTv: TextView? = null
-    var sevenTv: TextView? = null
-    var eightTv: TextView? = null
-    var nineTv: TextView? = null
-    var doubleZeroTv: TextView? = null
-    var zeroTv: TextView? = null
-    var dotTv: TextView? = null
-    var clearAllTv: TextView? = null
-    var clearTv: TextView? = null
-    var divideTv: TextView? = null
-    var intoTv: TextView? = null
-    var minusTv: TextView? = null
-    var plusTv: TextView? = null
-    var equalsTv: TextView? = null
     var calculatorViewModel: CalculatorViewModel? = null
+    private var currentTextLiveData: MutableLiveData<String>? = null
+    private val TAG = "CalculatorActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mCalculatorBinding = DataBindingUtil.setContentView(this, R.layout.activity_calculator)
+        setContentView(R.layout.activity_calculator)
+
         calculatorViewModel = ViewModelProviders.of(this).get(CalculatorViewModel::class.java)
-        mCalculatorBinding.let { binding ->
-            calculateTv = binding?.calculateTv
-            resultTv = binding?.resultTv
-            oneTv = binding?.oneTv
-            twoTv = binding?.twoTv
-            threeTv = binding?.twoTv
-            fourTv = binding?.twoTv
-            fiveTv = binding?.twoTv
-            sixTv = binding?.twoTv
-            sevenTv = binding?.twoTv
-            eightTv = binding?.twoTv
-            nineTv = binding?.twoTv
-            doubleZeroTv = binding?.twoTv
-            zeroTv = binding?.twoTv
-            dotTv = binding?.twoTv
-            clearAllTv = binding?.twoTv
-            clearTv = binding?.twoTv
-            divideTv = binding?.twoTv
-            intoTv = binding?.twoTv
-            minusTv = binding?.twoTv
-            plusTv = binding?.twoTv
 
+        currentTextLiveData = calculatorViewModel?.getCurrentTextLiveData()
+        currentTextLiveData?.observe(this) {
+            calculatorViewModel?.currentText = it
+            calculate_tv.text = it
+        }
 
-            oneTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val currentText: String? = calculatorViewModel!!.currentText
-                    calculatorViewModel!!.currentText = currentText + "1"
-                }
-            })
+        calculatorViewModel?.getAnswerTextLiveData()?.observe(this) {
+            result_tv.text = it
+        }
 
-            twoTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val currentText: String? = calculatorViewModel!!.currentText
-                    calculatorViewModel!!.currentText = currentText + "2"
-                }
-            })
+        one_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentTextLiveData?.postValue(currentText + "1")
+        }
 
-            threeTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val currentText: String? = calculatorViewModel!!.currentText
-                    calculatorViewModel!!.currentText = currentText + "3"
-                }
-            })
+        two_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentTextLiveData?.postValue(currentText + "2")
+        }
 
-            fourTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val currentText: String? = calculatorViewModel!!.currentText
-                    calculatorViewModel!!.currentText = currentText + "4"
-                }
-            })
+        three_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentTextLiveData?.postValue(currentText + "3")
+        }
 
-            fiveTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val currentText: String? = calculatorViewModel!!.currentText
-                    calculatorViewModel!!.currentText = currentText + "5"
-                }
-            })
+        four_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentTextLiveData?.postValue(currentText + "4")
+        }
 
-            sixTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val currentText: String? = calculatorViewModel!!.currentText
-                    calculatorViewModel!!.currentText = currentText + "6"
-                }
-            })
+        five_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentTextLiveData?.postValue(currentText + "5")
+        }
 
-            sevenTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val currentText: String? = calculatorViewModel!!.currentText
-                    calculatorViewModel!!.currentText = currentText + "7"
-                }
-            })
+        six_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentTextLiveData?.postValue(currentText + "6")
+        }
 
-            eightTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val currentText: String? = calculatorViewModel!!.currentText
-                    calculatorViewModel!!.currentText = currentText + "8"
-                }
-            })
+        seven_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentTextLiveData?.postValue(currentText + "7")
+        }
 
-            nineTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val currentText: String? = calculatorViewModel!!.currentText
-                    calculatorViewModel!!.currentText = currentText + "9"
-                }
-            })
+        eight_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentTextLiveData?.postValue(currentText + "8")
+        }
 
-            zeroTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val currentText: String? = calculatorViewModel!!.currentText
-                    calculatorViewModel!!.currentText = currentText + "0"
-                }
-            })
+        nine_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentTextLiveData?.postValue(currentText + "9")
+        }
 
-            doubleZeroTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val currentText: String? = calculatorViewModel!!.currentText
-                    calculatorViewModel!!.currentText = currentText + "00"
-                }
-            })
+        zero_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentTextLiveData?.postValue(currentText + "0")
+        }
 
-            clearTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val currentText: String? = calculatorViewModel?.currentText
-                    if (currentText != null && currentText.isNotEmpty()) {
-                        calculatorViewModel?.currentText =
-                            currentText.substring(0, currentText.length - 1)
+        double_zero_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentTextLiveData?.postValue(currentText + "00")
+        }
+
+        clear_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel?.currentText
+            if (currentText != null && currentText.isNotEmpty()) {
+                currentTextLiveData?.postValue(currentText.substring(0, currentText.length - 1))
+            }
+        }
+
+        into_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentText?.let {
+                when {
+                    !(currentText.endsWith("x") ||
+                            currentText.endsWith("÷") ||
+                            currentText.endsWith("+") ||
+                            currentText.endsWith("-")) -> {
+                        currentTextLiveData?.postValue(currentText + "x")
                     }
                 }
-            })
+            }
+        }
 
-            clearAllTv?.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    calculatorViewModel?.currentText = ""
+        divide_all_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentText?.let {
+                when {
+                    !(currentText.endsWith("x") ||
+                            currentText.endsWith("÷") ||
+                            currentText.endsWith("+") ||
+                            currentText.endsWith("-")) -> {
+                        currentTextLiveData?.postValue("$currentText÷")
+                    }
                 }
-            })
+            }
+        }
 
+        add_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentText?.let {
+                when {
+                    !(currentText.endsWith("x") ||
+                            currentText.endsWith("÷") ||
+                            currentText.endsWith("+") ||
+                            currentText.endsWith("-")) -> {
+                        currentTextLiveData?.postValue("$currentText+")
+                    }
+                }
+            }
+        }
 
+        minus_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentText?.let {
+                when {
+                    !(currentText.endsWith("x") ||
+                            currentText.endsWith("÷") ||
+                            currentText.endsWith("+") ||
+                            currentText.endsWith("-")) -> {
+                        currentTextLiveData?.postValue("$currentText-")
+                    }
+                }
+            }
+        }
+
+        switch_tv.setOnClickListener {
+            val value: String = result_tv.text as String
+            var isDouble = false
+            try {
+                value.toDouble()
+                isDouble = true
+            } catch (e: Exception) {
+            }
+
+            if (isDouble)
+                currentTextLiveData?.postValue(result_tv.text as String?)
+            else
+                currentTextLiveData?.postValue("0")
+        }
+
+        dot_tv.setOnClickListener {
+            val currentText: String? = calculatorViewModel!!.currentText
+
+            currentText?.let {
+                when {
+                    !it.endsWith(".") -> {
+                        if (it.contains("x") ||
+                            it.contains("÷") ||
+                            it.contains("+") ||
+                            it.contains("-")
+                        ) {
+                            val newCurrentTextMultiply: String =
+                                it.substring(it.lastIndexOf("x") + 1)
+                            val newCurrentTextDivide: String = it.substring(it.lastIndexOf("÷") + 1)
+                            val newCurrentTextAdd: String = it.substring(it.lastIndexOf("+") + 1)
+                            val newCurrentTextMinus: String = it.substring(it.lastIndexOf("-") + 1)
+
+                            var hasCorrectValue = ""
+
+                            try {
+                                newCurrentTextMultiply.toDouble();
+                                hasCorrectValue = "x"
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                            }
+
+                            try {
+                                newCurrentTextDivide.toDouble();
+                                hasCorrectValue = "/"
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                            }
+
+                            try {
+                                newCurrentTextAdd.toDouble();
+                                hasCorrectValue = "+"
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                            }
+
+                            try {
+                                newCurrentTextMinus.toDouble();
+                                hasCorrectValue = "-"
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                            }
+                            Log.d(TAG, "onCreate hasCorrectValue: $hasCorrectValue")
+                            if (TextUtils.isEmpty(hasCorrectValue)) {
+                                currentTextLiveData?.postValue("$it.")
+                            } else {
+                                when (hasCorrectValue) {
+                                    "x" -> {
+                                        if (newCurrentTextMultiply.contains(".0") || TextUtils.isDigitsOnly(
+                                                newCurrentTextMultiply
+                                            )
+                                        ) {
+                                            currentTextLiveData?.postValue("$it.")
+                                        }
+                                    }
+                                    "/" -> {
+                                        if (newCurrentTextDivide.contains(".0") || TextUtils.isDigitsOnly(
+                                                newCurrentTextDivide
+                                            )
+                                        ) {
+                                            currentTextLiveData?.postValue("$it.")
+                                        }
+                                    }
+                                    "+" -> {
+                                        if (newCurrentTextAdd.contains(".0") || TextUtils.isDigitsOnly(
+                                                newCurrentTextAdd
+                                            )
+                                        ) {
+                                            currentTextLiveData?.postValue("$it.")
+                                        }
+                                    }
+                                    "-" -> {
+                                        if (newCurrentTextMinus.contains(".0") || TextUtils.isDigitsOnly(
+                                                newCurrentTextMinus
+                                            )
+                                        ) {
+                                            currentTextLiveData?.postValue("$it.")
+                                        }
+                                    }
+                                }
+                            }
+
+                            Log.d(TAG, "onCreate newCurrentTextMultiply: $newCurrentTextMultiply")
+                            Log.d(TAG, "onCreate newCurrentTextDivide: $newCurrentTextDivide")
+                            Log.d(TAG, "onCreate newCurrentTextAdd: $newCurrentTextAdd")
+                            Log.d(TAG, "onCreate newCurrentTextMinus: $newCurrentTextMinus")
+                        } else {
+                            currentTextLiveData?.postValue("$it.")
+                        }
+                    }
+                }
+            }
+        }
+
+        equals_tv.setOnClickListener {
+            calculatorViewModel!!.currentText?.let {
+                if (it.endsWith("x") ||
+                    it.endsWith("÷") ||
+                    it.endsWith("+") ||
+                    it.endsWith("-") ||
+                    it.endsWith(".")
+                ) {
+                    currentTextLiveData?.postValue(it + "0")
+                    calculatorViewModel!!.currentText = it + "0"
+                }
+            }
+            val currentText: String? = calculatorViewModel!!.currentText
+            currentText?.let { it1 -> calculatorViewModel?.eval(it1) }
+        }
+
+        clear_all_tv.setOnClickListener() {
+            currentTextLiveData?.postValue("")
+            result_tv.text = "Ans"
         }
 
 
